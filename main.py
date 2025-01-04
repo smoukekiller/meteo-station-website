71f186727fa74fd59002bb9337ef897ddcbec371
smoukekiller <4406248@gmail.com>
2025-01-04 22:03:39 +0200

    talisman

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from flask_talisman import Talisman
 from get_data import get_data
-app = Flask(__name__)
 
+app = Flask(__name__)
+Talisman(app)
 @app.route("/")
 def home():
     temperature, humidity = get_data()
     return render_template("index.html", temperature=temperature, humidity=humidity)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000)
+    app.run(host="127.0.0.1", port=443)
